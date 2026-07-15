0b6fbac3cf7e8a614ed8ec77b24b2f512827ab5c
Miron Maria <maria-carmina.miron@student.tuiasi.ro>
2026-07-15 13:56:01 +0300

    Toate rezorvarile

--- Intro/set_exercitii_marti.py
+++ Intro/set_exercitii_marti.py
@@ -27,9 +27,10 @@ def palindrom(x:int):
         n=n//10
         ogl=ogl*10+c
     if x==ogl:
-        print('numarul este palindrom')
+
+        return True
     else:
-        print('numarul nu este palindrom')
+        return False
 
 def este_prim(n):
     if n <= 1:
@@ -62,6 +63,42 @@ def medii(lista):
     mg=math.sqrt(min*max)
     print("Media geometrica a numerelor din lista este: ", mg)
 
+def min_si_max(lista):
+    mijloc=len(lista)//2
+    max_list=lista[:mijloc]
+    min_list=lista[mijloc:]
+    maximul=max(max_list)
+    minim=min(min_list)
+    print('Maximul din prima jumatate a listei ', maximul)
+    print('Minimul din din a doua jumatate a listei ',minim)
+
+def palindrom_si_par(lista):
+    rezultat=[]
+    for i in range(0,len(lista)):
+        ok=palindrom(lista[i])
+        sir_numar = str(abs(lista[i]))
+        if ok==True and len(sir_numar) % 2 == 0:
+            rezultat.append(lista[i])
+    return rezultat
+def sublista_intre_min_si_max(lista):
+
+    valoare_min = min(lista)
+    valoare_max = max(lista)
+
+    index_min = lista.index(valoare_min)
+    index_max = lista.index(valoare_max)
+
+    start = min(index_min, index_max)
+    end = max(index_min, index_max)
+
+    return lista[start : end + 1]
+
+
+def diagonala_sortata(matrice):
+
+    diagonala = [matrice[i][i] for i in range(len(matrice))]
+    return sorted(diagonala, reverse=True)
+
 def main():
     print('ex5')
     x=2**1024
@@ -81,9 +118,17 @@ def main():
 
     print('ex8')
     x=input('Intoduceti numarul pentru a verifica daca e palindrom')
-    palindrom(int(x))
+    ok=palindrom(int(x))
+    if ok==True:
+        print('numarul este palindrom')
+    else:
+        print('numarul nu este palindrom')
     x = input('Intoduceti numarul pentru a verifica daca e palindrom')
-    palindrom(int(x))
+    ok=palindrom(int(x))
+    if ok==True:
+        print('numarul este palindrom')
+    else:
+        print('numarul nu este palindrom')
 
     print('ex9')
     prim=este_prim(a)
@@ -93,11 +138,24 @@ def main():
         print('Numarul ',a,' este prim')
 
     print('ex10')
-    lista=[1,4,2,6,78]
+    lista=[4,2,7,6,78,5]
     afisare_lista(lista)
 
     print('ex11')
     medii(lista)
+    print('ex12')
+    min_si_max(lista)
+
+    print('ex13')
+    numere = [121, 1221, 3443, 56, 123, 77, 8888]
+    print(palindrom_si_par(numere))
+
+    print('ex14')
+    print(sublista_intre_min_si_max(lista))
+
+    print('ex15')
+    matrice = [[10, 2, 3],[4, 50, 6],[7, 8, 5]]
+    print(diagonala_sortata(matrice))
 
 if __name__ == "__main__":
     main()
